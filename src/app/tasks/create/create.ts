1ae5ef4af4620d03421c16b09931a9d46867497c
curiouskorokke <nicholasngshihhao@gmail.com>
2025-07-01 16:02:53 +1200

    create new task component

---
+++ src/app/tasks/create/create.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TaskInterface } from '../task/task.model';
+
+@Component({
+  selector: 'app-task-create',
+  imports: [FormsModule],
+  templateUrl: './create.html',
+  styleUrl: './create.css',
+})
+export class TaskCreateComponent {
+  @Input({ required: true }) userId!: string;
+  @Output() dialogClose = new EventEmitter<void>();
+  @Output() newTaskCreate = new EventEmitter<TaskInterface>();
+  inputTitle = '';
+  inputSummary = '';
+  inputDate = '';
+
+  onClose() {
+    this.dialogClose.emit();
+  }
+
+  createNewTask() {
+    this.newTaskCreate.emit({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: this.inputTitle,
+      summary: this.inputSummary,
+      dueDate: this.inputDate,
+    });
+  }
+}
