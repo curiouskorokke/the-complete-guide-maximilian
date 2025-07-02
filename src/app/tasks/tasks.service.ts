634cd73b8e5e619a46d170c06d535e194d0071cc
curiouskorokke <nicholasngshihhao@gmail.com>
2025-07-02 16:56:18 +1000

    using local storage to store tasks

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
 import { TaskInterface } from './task/task.model';
-import { DUMMY_TASKS } from './tasks.mock-data';
 
 @Injectable()
 export class TasksService {
-  tasks: TaskInterface[] = DUMMY_TASKS;
+  tasks: TaskInterface[] = [];
+
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (!tasks) return;
+
+    this.tasks = JSON.parse(tasks);
+  }
   getTasks(userId: string): TaskInterface[] {
     return this.tasks.filter((task) => task.userId === userId);
   }
 
   deleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.setTasksToLocal();
   }
 
   addTask(task: TaskInterface) {
     this.tasks.push(task);
+    this.setTasksToLocal();
+  }
+
+  setTasksToLocal() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
